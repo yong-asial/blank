0f1d7cf08cf06f2d7dc3e8be8ec66153a2e48629
Masahiro Tanaka <masahiro@asial.co.jp>
2016-04-28 18:02:21 +0900

    Some change

--- www/components/loader.js
+++ www/components/loader.js
@@ -9,6 +9,26 @@
 /*** <Start:monaca-cordova-loader> ***/
 /*** <Start:monaca-cordova-loader LoadJs:"components/monaca-cordova-loader/cordova-loader.js"> ***/
 (function(){
+  function getDeviceObjectForPreview() {
+    var raw_values = window.location.search.substring(1).split('&');
+    var values = {};
+    var device = { platform: "" };
+    
+    if (raw_values) {
+      for (var key in raw_values) {
+        var tmp = raw_values[key].split('=');
+        values[tmp[0]] = decodeURIComponent(tmp[1]);
+      }
+      device.platform = values.platform;
+    }
+    
+    return device;
+  }
+    
+  if (/^https:\/\/preview-.+monaca\.(local||mobi)/.test(location.href)) {
+    window.device = getDeviceObjectForPreview();
+  }
+ 
   if ((navigator.userAgent.match(/Android/i)) || (navigator.userAgent.match(/iPhone|iPad|iPod/i))) {
     if (typeof location.href === "string") {
       var relativePath = location.href.split("/www")[1];
@@ -19,7 +39,7 @@
       }
       document.write("<script src=\"" + cordovaJsUrl+ "cordova.js" + "\"></script>");
     }
-  } else if ((navigator.userAgent.match(/MSIE\s10.0/)) && (navigator.userAgent.match(/Windows\sNT\s6.2/))) {
+  } else if ( ((navigator.userAgent.match(/MSIE\s10.0/)) && (navigator.userAgent.match(/Windows\sNT\s6.2/)) ) || navigator.userAgent.match(/MSAppHost/) ) {
     var elm = document.createElement('script');
     elm.setAttribute("src", "cordova.js");
     document.getElementsByTagName("head")[0].appendChild(elm);
